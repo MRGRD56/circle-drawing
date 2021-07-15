1debd54e4cf2a37ca92b3ddf04e9bb87519773bd
MRGRD56-neon <bukinist85@gmail.com>
2021-07-15 13:06:17 +0500

    one fix

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,8 @@ function drawCircle(canvas: HTMLCanvasElement, radius: number, pointsCount: numb
 
     const diameter = radius * 2;
     const canvasSize = new Size(
-        margin.left + margin.right + diameter + pointsWidth * 3,
-        margin.top + margin.bottom + diameter + pointsWidth * 3);
+        margin.left + margin.right + diameter + pointsWidth * 2,
+        margin.top + margin.bottom + diameter + pointsWidth * 2);
 
     [canvas.width, canvas.height] = [canvasSize.width, canvasSize.height]
 
@@ -20,7 +20,7 @@ function drawCircle(canvas: HTMLCanvasElement, radius: number, pointsCount: numb
         return;
     }
 
-    const centerPoint = new Point(radius + margin.left + pointsWidth * 1.5, radius + margin.top + pointsWidth * 1.5);
+    const centerPoint = new Point(radius + margin.left + pointsWidth, radius + margin.top + pointsWidth);
     const pointsDistance = 360 / pointsCount;
 
     const drawPoint = (point: Point, width: number, color: string) => {
@@ -28,9 +28,9 @@ function drawCircle(canvas: HTMLCanvasElement, radius: number, pointsCount: numb
         canvasContext.arc(point.x, point.y, width, 0, 2 * Math.PI, false);
         canvasContext.fillStyle = color;
         canvasContext.fill();
-        canvasContext.lineWidth = width;
-        canvasContext.strokeStyle = color;
-        canvasContext.stroke();
+        // canvasContext.lineWidth = width;
+        // canvasContext.strokeStyle = color;
+        // canvasContext.stroke();
     }
 
     for (let point = 0; point < pointsCount; point++) {
@@ -39,7 +39,6 @@ function drawCircle(canvas: HTMLCanvasElement, radius: number, pointsCount: numb
         const shift = new Point(Math.cos(radAngle) * radius, Math.sin(radAngle) * radius);
 
         const pointPosition = new Point(centerPoint.x + shift.x, centerPoint.y + shift.y);
-        //canvasContext!.fillRect(pointPosition.x, pointPosition.y, pointsWidth, pointsWidth);
         drawPoint(pointPosition, pointsWidth, pointsColor);
     }
 }
@@ -60,8 +59,8 @@ setInterval(() => {
         circleCanvas,
         300,
         i,
-        5,
+        8,
         "#2196f3",
-        Thickness.factory.createThickness2(10, 20));
+        Thickness.factory.createThickness1(10));
     i += increment;
 }, 50)
